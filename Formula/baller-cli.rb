2206aa208ea5abeda2d0f897a1594c84babb2cc3
whoop-t <tommymallis@gmail.com>
2022-06-26 10:58:27 -0700

    Release 0.1.1

--- Formula/baller-cli.rb
+++ Formula/baller-cli.rb
@@ -4,9 +4,9 @@
 class BallerCli < Formula
   desc "Baller cli for fast pr creation"
   homepage "https://github.com/whoop-t/baller-cli"
-  url "https://github.com/whoop-t/baller-cli/releases/download/Beta/baller-mac.tar.gz"
+  url "https://github.com/whoop-t/baller-cli/releases/download/Beta.1/baller-mac.tar.gz"
   sha256 "c499a4f64bde7921f223d2408bf373660365f3f9a0f21f2a5d656cb3fef2e005"
-  version "0.1.0"
+  version "0.1.1"
 
   def install
     bin.install "baller"
